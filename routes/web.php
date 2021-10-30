0a387fb21d928540fb9fe2c1c833526d59906fd9
Ethan Morgue <nahum.ramirez@hotmail.com>
2021-10-29 19:18:49 -0500

    se agrego el template de guest al proyecto y el diagrama de base de datos

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.guest.base');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
