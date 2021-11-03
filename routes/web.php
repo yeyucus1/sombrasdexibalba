6840da215160087ed44bbd30201bcaa2bd3a73ef
Yeyo <sergiodejesus@itsn.edu.mx>
2021-11-02 22:58:19 -0600

    Inicie el template de admin y la vista pgAdmin

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/admin', function () {
+    return view('layouts.pgAdmin.pgAdmin');
+});
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+
