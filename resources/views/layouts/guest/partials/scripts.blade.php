d3e57d02104b003ae79606a69a7fdd1eba613236
Ethan Morgue <nahum.ramirez@hotmail.com>
2021-11-14 23:25:44 -0600

    Se actualizo el diagrama de base de datos

---
+++ resources/views/layouts/guest/partials/scripts.blade.php
@@ -0,0 +1,13 @@
+<div>
+    {{--  Hojas de estilo necesarias para el template  --}}
+    <link href="{{asset("template_guest/css/bootstrap.css")}}" rel="stylesheet">
+    <link href="{{asset("template_guest/css/main.css")}}" rel="stylesheet">
+
+    {{--  favicon de la pagina (icono pequeño que aparece en la pestaña de la pagina)  --}}
+    <link rel="shortcut icon" href="{{asset("template_guest/images/favicon.png")}}">
+
+    {{--  fuentes de google--}}
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,600' rel='stylesheet' type='text/css'>
+
+
+</div>
